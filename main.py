027ed562108901de2bb349c98e50253bc888eafc
Jona <jona@Jonathans-MacBook-Pro.local>
2020-12-03 17:07:13 -0500

    Add client upwork

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import os
 from dotenv import load_dotenv
+from pprint import pprint
 import upwork
-
 
 
 if __name__ == "__main__":
     ## Read environment varibales
     load_dotenv()
-    key = os.environ.get('KEY') 
-    secret = os.environ.get('SECRET') 
+    public_key = os.environ.get('KEY') 
+    secret_key = os.environ.get('SECRET') 
+
+    ## Login to api account
+    client = None
+    try:
+        client = upwork.Client(public_key, secret_key)
+        print(client.auth.get_info())
+        #pprint(client)
+        pass
+    except Exception as error:
+        print('error: ', error)
+        pass
+    finally:
+        print('everything ok')
+        pass
 
 
-    print(key)
-    print(secret)
     pass
